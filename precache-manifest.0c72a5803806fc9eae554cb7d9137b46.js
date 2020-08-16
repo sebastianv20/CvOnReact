b1da5d835122caf604a36fcbd87620cef80bb181
sebastianv20 <sebastianv20@gmail.com>
2020-08-16 17:15:06 -0300

    Updates

---
+++ precache-manifest.0c72a5803806fc9eae554cb7d9137b46.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69ba7e36aab687a496002638119176be",
+    "url": "/CvOnReact/index.html"
+  },
+  {
+    "revision": "50d1684b7168204d32be",
+    "url": "/CvOnReact/static/css/2.ac1ab58b.chunk.css"
+  },
+  {
+    "revision": "626051ac3ab232ebc0dc",
+    "url": "/CvOnReact/static/css/main.01ee8544.chunk.css"
+  },
+  {
+    "revision": "50d1684b7168204d32be",
+    "url": "/CvOnReact/static/js/2.ce6f8d85.chunk.js"
+  },
+  {
+    "revision": "4a1691fd44116c5f8c99a494e6966d73",
+    "url": "/CvOnReact/static/js/2.ce6f8d85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "626051ac3ab232ebc0dc",
+    "url": "/CvOnReact/static/js/main.74d68a60.chunk.js"
+  },
+  {
+    "revision": "b8c25168fc9e8569bc35",
+    "url": "/CvOnReact/static/js/runtime-main.88d8c806.js"
+  },
+  {
+    "revision": "82672221b727211397a4ebd3f1a984b3",
+    "url": "/CvOnReact/static/media/about.82672221.jpg"
+  },
+  {
+    "revision": "ee371d0a1910a6c23746b86544711f54",
+    "url": "/CvOnReact/static/media/encuestas.ee371d0a.png"
+  },
+  {
+    "revision": "5f1d6681ff898782cc3b9df585c17c22",
+    "url": "/CvOnReact/static/media/encuestas1.5f1d6681.png"
+  },
+  {
+    "revision": "de647ffdecb2af20aafc68f1fd23c417",
+    "url": "/CvOnReact/static/media/encuestas2.de647ffd.png"
+  },
+  {
+    "revision": "cf3c4178751386c022cee6a0b5db7186",
+    "url": "/CvOnReact/static/media/encuestas3.cf3c4178.png"
+  },
+  {
+    "revision": "ec63fbf8e57c9f3c1a895c6f9b2445ba",
+    "url": "/CvOnReact/static/media/firmas.ec63fbf8.JPG"
+  },
+  {
+    "revision": "f9aa1a1cc8a9acbd8b7dda7b8a60889e",
+    "url": "/CvOnReact/static/media/firmas2.f9aa1a1c.JPG"
+  },
+  {
+    "revision": "826cc1c57b61b0f1292924dfa1d6c881",
+    "url": "/CvOnReact/static/media/firmas2_TH.826cc1c5.jpg"
+  },
+  {
+    "revision": "787abc989683d793c2d2f079d10da60e",
+    "url": "/CvOnReact/static/media/firmas_TH.787abc98.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/CvOnReact/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "963bb857cb09160b12b6bc4095e03795",
+    "url": "/CvOnReact/static/media/videoconferencia.963bb857.JPG"
+  },
+  {
+    "revision": "81cd868784d571f5a046667de892756e",
+    "url": "/CvOnReact/static/media/videoconferencia_TH.81cd8687.jpg"
+  }
+]);
